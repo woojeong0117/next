cd7d52e3c715ea1cc8383d2f474501e8f6627b42
정민호 <jeongminho@jeongminhoui-MacBookAir.local>
2023-08-02 22:14:38 +0900

    1일차

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    assetPrefix:
+      process.env.NODE_ENV === "production"
+        ? "https://boramyy.github.io/invitation"
+        : "",
+  };
